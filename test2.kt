7049c968c64ccd314e77a69a314284b8b05ee046
hunglengc <167834050+hunglengc@users.noreply.github.com>
2024-05-11 14:35:11 +0700

    Add files via upload

---
+++ test2.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val radius = 5.5
+    val pi = 3.14
+    val volume = 0.75 * pi * radius * radius * radius
+    println("the volume is: 0.75*pi*radius^3")
+    println("the volume of the sphere with $radius and $volume")
+}
